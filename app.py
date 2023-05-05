68a6b8423c36ed264e02b1f59585c7306692173c
Danil <alvicov@mail.ru>
2023-05-05 16:55:08 +0300

    version1_alphatest

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, send_file
+from rembg import remove
+import io
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    file = request.files['file'].read()
+    result = remove(file)
+    result_image = io.BytesIO(result)
+    result_image.seek(0)
+    return send_file(result_image, mimetype='image/png')
+
+
+if __name__ == '__main__':
+    app.run()
